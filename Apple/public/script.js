7467e5c81e9c3d2631e988b18ec84233995633bf
mayminzz <mayminzz@naver.com>
2024-03-08 18:55:06 +0900

    포트폴리오 내 작업물

---
+++ Apple/public/script.js
@@ -0,0 +1,36 @@
+const header = document.querySelector("header");
+const gnbLists = document.querySelectorAll(".gnb_list");
+const detailBoxes = document.querySelectorAll(".detail_list_box");
+const main = document.querySelector("main");
+
+mouseenterEffect = (e) => {
+  const detailBox = e.currentTarget.querySelector(".detail_list_box");
+  detailBoxes.forEach((detailBox) => {
+    detailBox.classList.remove("active");
+  });
+  detailBox.classList.add("active");
+  main.classList.add("blurEffect");
+};
+
+gnbLists.forEach((list) => {
+  list.addEventListener("mouseenter", mouseenterEffect);
+});
+
+// 헤더 영역에서 마우스 이탈 이벤트 추가
+header.addEventListener("mouseleave", () => {
+  const detailBoxes = document.querySelectorAll(".detail_list_box");
+  detailBoxes.forEach((detailBox) => {
+    detailBox.classList.remove("active");
+  });
+  main.classList.remove("blurEffect");
+});
+
+//mobile trigger
+const triggertBtn = document.querySelector(".trigger");
+const gnbModal = document.querySelector(".gnb");
+
+triggertBtn.addEventListener("click", () => {
+  triggertBtn.classList.toggle("active");
+  gnbModal.classList.toggle("on");
+  main.classList.toggle("disappear");
+});
